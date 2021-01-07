2c72232289b150bd4890d94eced8cca8fbc15af9
Simon Iversen <simon.iversen@protonmail.com>
2021-01-07 20:57:40 +0100

    Misunderstood the exercise text. Problem solved.

--- ex_1-16.c
+++ ex_1-16.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#define MAXLINE 5 // longest number string - 2 since '\n' and '\0' fills the two last slots.
+#define MAXLINE 10 // longest number string - 2 since '\n' and '\0' fills the two last slots.
 
 int get_line(char line[], int maxline); // renamed to avoid naming conflict from <stdio>
-int get_line2(char line[], int maxline); 
 void copy(char to[], char from[]);
-void copy2(char to[], char from[], int lim);
 
 int main() {
   int len;
@@ -13,14 +11,14 @@ int main() {
   char longest[MAXLINE];
 
   max = 0;
-  while ((len = get_line2(line, MAXLINE)) > 0) {
+  while ((len = get_line(line, MAXLINE)) > 0) {
     if (len > max) {
       max = len;
       copy(longest, line);
     }
   }
   if (max > 0) // there was a line!
-    printf("Longest line: %s\n", longest);
+    printf("Length of longest line: %d\nLongest line: %s\n", max, longest);
   return 0;
 }
 
@@ -28,49 +26,30 @@ int main() {
 // read a line into s, return length
 int get_line(char s[], int lim)
 {
-  int c, i;
-  for (i=0; i < lim - 1 && (c = getchar()) != EOF && c!='\n'; ++i)
-  {
-    s[i] = c;
-  }
-  if (c == '\n')
-  {
-    s[i] = c;
-    ++i; // Q: difference between prefix and postfix
-  }
-  s[i] = '\0'; // terminates string in C
-  return i;
-}
-
-int get_line2(char s[], int lim)
-{
+  int flag = 0;
   int c, i;
   for (i=0; (c = getchar()) != EOF && c!='\n'; ++i)
   {
-    
-    if (i >= lim - 1)
-    {
-      unsigned new_size = lim + MAXLINE;
-      char tmp[new_size];
-      copy2(tmp, s, lim);
-      lim = lim + MAXLINE; // TODO: works, but greasy
-      printf("s: %s, tmp: %s\n", s, tmp);
-      s = tmp; // TODO: sketchy step?!
-      printf("s: %s, tmp: %s\n", s, tmp);
-    }
-    
-    s[i] = c;
-    ++i; 
+    if (i < lim - 1)
+      s[i] = c;
+    else
+      flag = 1;
   }
   if (c == '\n')
   {
-    s[i] = c;
+    if (flag == 1) {
+      s[lim - 2] = '\n';
+      s[lim - 1] = '\0';
+    }
+    else {
+      s[i] = '\n';
+      s[i] = '\0';
+    }
     ++i;
   }
-  s[i] = '\0'; // terminates string in C
+  //s[i] = '\0'; // terminates string in C
   return i;
 }
-
 
 void copy(char to[], char from[]) {
   int i;
@@ -79,18 +58,4 @@ void copy(char to[], char from[]) {
   while ((to[i] = from[i]) != '\0') {
     ++i;
   }
-}
-
-// modified copy function to increase size of array, leaving empty room at the end.
-// Remember that arrays are called by reference and not by local copy.
-void copy2(char to[], char from[], int lim) {
-  int i;
-
-  i = 0;
-  
-  while (i < lim - 1)
-  {
-    to[i] = from[i];
-    ++i;
-  }    
 }
