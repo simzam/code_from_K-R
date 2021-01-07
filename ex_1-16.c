f35e7b67244896a5de5cab8d48a7dd61bbf01021
Simon Iversen <simon.iversen@protonmail.com>
2021-01-07 19:33:22 +0100

    modified copy function does not work!

--- ex_1-16.c
+++ ex_1-16.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#define MAXLINE 10
+#define MAXLINE 5 // longest number string - 2 since '\n' and '\0' fills the two last slots.
 
-int getline(char line[], int maxline);
+int get_line(char line[], int maxline); // renamed to avoid naming conflict from <stdio>
+int get_line2(char line[], int maxline); 
 void copy(char to[], char from[]);
+void copy2(char to[], char from[], int lim);
 
 int main() {
   int len;
@@ -11,35 +13,54 @@ int main() {
   char longest[MAXLINE];
 
   max = 0;
-  while ((len = getline(line, MAXLINE)) > 0) {
+  while ((len = get_line2(line, MAXLINE)) > 0) {
     if (len > max) {
       max = len;
       copy(longest, line);
     }
-    if (max > 0) // there was a line!
-      printf("%s\n", longest);
   }
+  if (max > 0) // there was a line!
+    printf("Longest line: %s\n", longest);
   return 0;
 }
 
 // Q: Necessary to use different name than in declaration above?
 // read a line into s, return length
-int getline(char s[], int lim)
+int get_line(char s[], int lim)
 {
   int c, i;
-  for (i=0; i < lim - 1 &&(c = getchar()) != EOF && c!='\n'; ++i)
+  for (i=0; i < lim - 1 && (c = getchar()) != EOF && c!='\n'; ++i)
   {
-    /*
+    s[i] = c;
+  }
+  if (c == '\n')
+  {
+    s[i] = c;
+    ++i; // Q: difference between prefix and postfix
+  }
+  s[i] = '\0'; // terminates string in C
+  return i;
+}
+
+int get_line2(char s[], int lim)
+{
+  int c, i;
+  for (i=0; (c = getchar()) != EOF && c!='\n'; ++i)
+  {
+    
     if (i >= lim - 1)
     {
       unsigned new_size = lim + MAXLINE;
       char tmp[new_size];
-      copy(tmp, s, new_size - MAXLINE);
-      s = tmp;
+      copy2(tmp, s, lim);
+      lim = lim + MAXLINE; // TODO: works, but greasy
+      printf("s: %s, tmp: %s\n", s, tmp);
+      s = tmp; // TODO: sketchy step?!
+      printf("s: %s, tmp: %s\n", s, tmp);
     }
-    */
+    
     s[i] = c;
-    ++i; // Q: difference between prefix and postfix
+    ++i; 
   }
   if (c == '\n')
   {
@@ -50,6 +71,7 @@ int getline(char s[], int lim)
   return i;
 }
 
+
 void copy(char to[], char from[]) {
   int i;
 
@@ -58,8 +80,9 @@ void copy(char to[], char from[]) {
     ++i;
   }
 }
-  
- 
+
+// modified copy function to increase size of array, leaving empty room at the end.
+// Remember that arrays are called by reference and not by local copy.
 void copy2(char to[], char from[], int lim) {
   int i;
 
