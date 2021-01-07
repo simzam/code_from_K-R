55375feb7c8dd7f270714651190331ba9dd883f0
Simon Iversen <simon.iversen@protonmail.com>
2021-01-07 18:41:10 +0100

    Reset file to match example in section 1.9 y

--- ex_1-16.c
+++ ex_1-16.c
@@ -2,7 +2,7 @@
 #define MAXLINE 10
 
 int getline(char line[], int maxline);
-void copy(char to[], char from[], int lim);
+void copy(char to[], char from[]);
 
 int main() {
   int len;
@@ -11,21 +11,23 @@ int main() {
   char longest[MAXLINE];
 
   max = 0;
-
   while ((len = getline(line, MAXLINE)) > 0) {
     if (len > max) {
       max = len;
       copy(longest, line);
-    
+    }
+    if (max > 0) // there was a line!
+      printf("%s\n", longest);
   }
   return 0;
 }
 
 // Q: Necessary to use different name than in declaration above?
+// read a line into s, return length
 int getline(char s[], int lim)
 {
   int c, i;
-  for (i=0; (c = getchar()) != EOF && c!='\n'; ++i)
+  for (i=0; i < lim - 1 &&(c = getchar()) != EOF && c!='\n'; ++i)
   {
     /*
     if (i >= lim - 1)
@@ -48,8 +50,17 @@ int getline(char s[], int lim)
   return i;
 }
 
-void copy(char to[], char from[], int lim)
-{
+void copy(char to[], char from[]) {
+  int i;
+
+  i = 0;
+  while ((to[i] = from[i]) != '\0') {
+    ++i;
+  }
+}
+  
+ 
+void copy2(char to[], char from[], int lim) {
   int i;
 
   i = 0;
